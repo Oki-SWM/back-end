c7359b9af0200c0e93ad542bbd0f47762596b6dc
dsjinwongo <dsgo22jw@chungbuk.ac.kr>
2022-04-24 16:12:01 +0900

    Initial commit

---
+++ src/main/java/com/example/oki/OkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.oki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oki/OkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
