2a00a3e9f22c717d176fce81a0efbb71b1c60950
Choiyunh_ <svs101@naver.com>
2022-04-25 02:01:36 +0900

    Feat: Add Member entity

---
+++ src/main/java/com/example/oki/member/domain/Member.java
@@ -0,0 +1,51 @@
+package com.example.oki.member.domain;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String memberId;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
