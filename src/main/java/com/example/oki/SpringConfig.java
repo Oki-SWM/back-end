a6b9accfc8b15711e497c64ac2f263821f106f3b
Choiyunh_ <svs101@naver.com>
2022-04-27 01:26:24 +0900

    Feat: 회원가입 및 로그인 API(REST)

--- src/main/java/com/example/oki/SpringConfig.java
+++ src/main/java/com/example/oki/SpringConfig.java
@@ -4,6 +4,8 @@ import com.example.oki.Board.repository.BoardRepository;
 import com.example.oki.Board.repository.JpaBoardRepository;
 import com.example.oki.Board.repository.JpaLikeRepository;
 import com.example.oki.Board.repository.LikeRepository;
+import com.example.oki.member.repository.JpaMemberRepository;
+import com.example.oki.member.repository.MemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +18,9 @@ public class SpringConfig {
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+
+    @Bean
+    public MemberRepository memberRepository() { return new JpaMemberRepository(em); }
 
     @Bean
     public BoardRepository boardRepository() {
--- src/main/java/com/example/oki/member/controller/MemberController.java
+++ src/main/java/com/example/oki/member/controller/MemberController.java
@@ -1,22 +1,39 @@
 package com.example.oki.member.controller;
 
+import com.example.oki.message.Message;
 import com.example.oki.member.domain.Member;
 import com.example.oki.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+
+@RestController
 public class MemberController {
 
     @Autowired
     MemberService memberService;
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signUp(Member m) {
-        memberService.signUpMember(m);
+    public ResponseEntity<Message> signUp(@RequestBody Member m) {
+        if (memberService.signUpMember(m)) {
+            Message message = new Message(HttpStatus.OK, "Success", null);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+        Message message = new Message(HttpStatus.CONFLICT, "Duplicate id", null);
+        return new ResponseEntity<>(message, HttpStatus.CONFLICT);
+    }
 
-        return "redirect:/board";
+    @RequestMapping(value = "/signin", method = RequestMethod.POST)
+    public ResponseEntity<Message> signIn(@RequestBody Member m) {
+        Member loginMember = memberService.signInMember(m.getMemberId(), m.getPassword());
+
+        if (loginMember == null) {
+            Message message = new Message(HttpStatus.NOT_FOUND, "Incorrect Id or Password", null);
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        }
+        Message message = new Message(HttpStatus.OK, "Success", loginMember);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
