bf429c091ae03d44ef7749c40254f64a409422b9
Choiyunh_ <svs101@naver.com>
2022-04-27 18:33:24 +0900

    Feat: Add findById(PK)

--- src/main/java/com/example/oki/member/repository/JpaMemberRepository.java
+++ src/main/java/com/example/oki/member/repository/JpaMemberRepository.java
@@ -1,12 +1,14 @@
 package com.example.oki.member.repository;
 
 import com.example.oki.member.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class JpaMemberRepository implements MemberRepository {
 
     private final EntityManager em;
@@ -19,6 +21,11 @@ public class JpaMemberRepository implements MemberRepository {
     @Override
     public void save(Member member) {
         em.persist(member);
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
     }
 
     @Override
