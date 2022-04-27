fa561bf5f9718ca19edb74df4a2594bc742b7342
Choiyunh_ <svs101@naver.com>
2022-04-27 18:33:34 +0900

    Feat: Add findById(PK)

--- src/main/java/com/example/oki/member/repository/MemberRepository.java
+++ src/main/java/com/example/oki/member/repository/MemberRepository.java
@@ -1,14 +1,14 @@
 package com.example.oki.member.repository;
 
 import com.example.oki.member.domain.Member;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface MemberRepository {
 
     void save(Member member);
+
+    Optional<Member> findById(Long id);
 
     Optional<Member> findByMemberId(String memberId);
 }
