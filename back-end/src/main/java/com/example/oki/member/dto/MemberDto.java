5c41a9003d240e4cb25afd282255d154a9f3b243
Choiyunh_ <svs101@naver.com>
2022-04-29 22:11:32 +0900

    Feat: Add MemberDto

---
+++ back-end/src/main/java/com/example/oki/member/dto/MemberDto.java
@@ -0,0 +1,13 @@
+package com.example.oki.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberDto {
+
+    private String name;
+
+    private String memberId;
+
+    private String password;
+}
